882a8a47a25e3c4d0218302452f33b1c9fdfb6cb
abdulmajidabdalah <abdulmajidabdalah27@gmail.com>
2022-08-03 04:21:48 +0700

    redesign

--- app.js
+++ app.js
@@ -1,10 +1,7 @@
 function tampilkanWaktu() {
-  var tampilkanJam = new Date();
-  var waktu = new Date(tampilkanJam.getTime());
-  var sh = waktu.getHours().toString();
-  var sm = waktu.getMinutes().toString();
-  document.getElementById("jam").innerHTML = (sh.length == 1 ? "0" + sh : sh) + ":" + (sm.length == 1 ? "0" + sm : sm);
+    var tampilkanJam = new Date();
+    var waktu = new Date(tampilkanJam.getTime());
+    var sh = waktu.getHours().toString();
+    var sm = waktu.getMinutes().toString();
+    document.getElementById("jam").innerHTML = (sh.length == 1 ? "0" + sh : sh) + ":" + (sm.length == 1 ? "0" + sm : sm);
 }
-
-const nama = prompt("Input Your Name, Please..");
-nama = alert("Hallo " + nama);
